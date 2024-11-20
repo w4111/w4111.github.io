a2b22ec1626a9b23b3df3622ed3c713aed9851c6
Eugene Wu <ewu@cs.columbia.edu>
2024-11-20 15:48:35 -0500

    lazy commit msg

--- gendates.py
+++ gendates.py
@@ -4,14 +4,15 @@ from datetime import *
 
 
 holidays = list(map(parser.parse, [
-  "11/07/2022",
-  "11/08/2022",
-  "11/23/2022",
-  "11/24/2022",
-  "11/25/2022"
+    "02/17/2025",
+    "03/17/2025",
+    "03/18/2025",
+    "03/19/2025",
+    "03/20/2025",
+    "03/21/2025"
 ]))
-startday = parser.parse("09/06/2022")
-lastday = parser.parse("12/12/2022")
+startday = parser.parse("01/21/2025")
+lastday = parser.parse("05/05/2025")
 daysofweek = [1, 3]  # 0 is monday
 
 day = startday
